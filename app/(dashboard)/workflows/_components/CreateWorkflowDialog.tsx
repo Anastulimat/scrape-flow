b01e4c2c59fe1cc12e9d2c5092808da36ef9fded
Anas TULIMAT <anas.tulimat@orange.com>
2025-04-30 00:02:58 +0200

    feat: add create workflow schema and add first field to the form

--- app/(dashboard)/workflows/_components/CreateWorkflowDialog.tsx
+++ app/(dashboard)/workflows/_components/CreateWorkflowDialog.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import {z} from "zod";
 import {useState} from "react";
+import {useForm} from "react-hook-form";
 import {Layers2Icon} from "lucide-react";
 
 import {Button} from "@/components/ui/button";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {createWorkflowSchema} from "@/schema/workflows";
 import CustomDialogHeader from "@/components/CustomDialogHeader";
 import {Dialog, DialogContent, DialogTrigger} from "@/components/ui/dialog";
+import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
 
 
 // ----------------------------------------------------------------------
@@ -13,6 +19,11 @@ import {Dialog, DialogContent, DialogTrigger} from "@/components/ui/dialog";
 const CreateWorkflowDialog = ({triggerText}: { triggerText?: string }) => {
 
     const [open, setOpen] = useState(false);
+
+    const form = useForm<z.infer<typeof createWorkflowSchema>>({
+        resolver: zodResolver(createWorkflowSchema),
+        defaultValues: {}
+    });
 
     return (
         <Dialog open={open} onOpenChange={setOpen}>
@@ -26,6 +37,31 @@ const CreateWorkflowDialog = ({triggerText}: { triggerText?: string }) => {
                     title="Create workflow"
                     subTitle="Start building your workflow"
                 />
+                <div className="p-6">
+                    <Form {...form}>
+                        <form className="space-y-8 w-full">
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel className="flex gap-1 items-center">
+                                            Name
+                                            <p className="text-sm text-primary">(required)</p>
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input {...field} />
+                                        </FormControl>
+                                        <FormDescription>
+                                            Choose a descriptive and unique name
+                                        </FormDescription>
+                                        <FormMessage/>
+                                    </FormItem>
+                                )}
+                            />
+                        </form>
+                    </Form>
+                </div>
             </DialogContent>
         </Dialog>
     );
