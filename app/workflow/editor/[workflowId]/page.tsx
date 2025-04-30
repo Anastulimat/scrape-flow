03177cb590f4d595dcaed57720f2a4cff4080ead
Anas TULIMAT <anas.tulimat@orange.com>
2025-04-30 12:22:39 +0200

    feat: start working on react flow for the flow editor

---
+++ app/workflow/editor/[workflowId]/page.tsx
@@ -0,0 +1,33 @@
+import prisma from "@/lib/prisma";
+import {auth} from "@clerk/nextjs/server";
+import Editor from "../_components/Editor";
+
+// ----------------------------------------------------------------------
+
+const WorkflowEditor = async ({params}: { params: { workflowId: string } }) => {
+    const {workflowId} = params;
+
+    const {userId} = auth();
+    if (!userId) {
+        return <div>Unauthenticated</div>;
+    }
+
+
+    const workflow = await prisma.workflow.findUnique({
+        where: {
+            id: workflowId,
+            userId
+        },
+    });
+
+    if (!workflow) {
+        return <div>Workflow not found</div>;
+    }
+
+
+    return (
+        <Editor workflow={workflow} />
+    );
+};
+
+export default WorkflowEditor;
