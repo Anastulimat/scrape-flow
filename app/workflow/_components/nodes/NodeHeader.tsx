305e1b7340f6a2ba10f3eaf47c8e297f37f10ab8
Anas TULIMAT <anas.tulimat@orange.com>
2025-04-30 23:34:47 +0200

    feat: implement delete task from flow editor

--- app/workflow/_components/nodes/NodeHeader.tsx
+++ app/workflow/_components/nodes/NodeHeader.tsx
@@ -4,14 +4,16 @@ import React from 'react';
 import {TaskType} from "@/types/task";
 import {TaskRegistry} from "@/lib/workflow/task/Registry";
 import {Badge} from "@/components/ui/badge";
-import {CoinsIcon, GripVerticalIcon} from "lucide-react";
+import {CoinsIcon, CopyIcon, GripVerticalIcon, TrashIcon} from "lucide-react";
 import {Button} from "@/components/ui/button";
+import {useReactFlow} from "@xyflow/react";
 
 // ----------------------------------------------------------------------
 
 
-const NodeHeader = ({taskType}: { taskType: TaskType }) => {
+const NodeHeader = ({taskType, nodeId}: { taskType: TaskType, nodeId: string }) => {
     const task = TaskRegistry[taskType];
+    const {deleteElements} = useReactFlow();
 
     return (
         <div className="flex items-center gap-2 p-2">
@@ -23,9 +25,31 @@ const NodeHeader = ({taskType}: { taskType: TaskType }) => {
                 <div className="flex gap-1 items-center">
                     {task.isEntryPoint && <Badge>Entry point</Badge>}
                     <Badge className="gap-2 flex items-center text-xs">
-                        <CoinsIcon size={16} />
+                        <CoinsIcon size={16}/>
                         TODO
                     </Badge>
+                    {!task.isEntryPoint && (
+                        <>
+                            <Button
+                                variant="ghost"
+                                size="icon"
+                                className="cursor-pointer text-red-400 hover:text-red-500"
+                                onClick={() => deleteElements({
+                                    nodes: [{id: nodeId}]
+                                })}
+                            >
+                                <TrashIcon size={12}/>
+                            </Button>
+
+                            <Button
+                                variant="ghost"
+                                size="icon"
+                                className="cursor-pointer"
+                            >
+                                <CopyIcon size={12}/>
+                            </Button>
+                        </>
+                    )}
                     <Button
                         variant="ghost"
                         size="icon"
