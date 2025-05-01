94850fc5175c1598a1493a801f932feddd6cb9de
Anas TULIMAT <anas.tulimat@orange.com>
2025-05-01 23:06:48 +0200

    feat: add runWorkflow server action so it saves the workflow execution plan into the database to be executed later

--- app/workflow/_components/topbar/ExecuteBtn.tsx
+++ app/workflow/_components/topbar/ExecuteBtn.tsx
@@ -3,6 +3,10 @@
 import {Button} from "@/components/ui/button";
 import {PlayIcon} from "lucide-react";
 import useExecutionPlan from "@/components/hooks/useExecutionPlan";
+import {useMutation} from "@tanstack/react-query";
+import {RunWorkflow} from "@/actions/workflows/runWorkflow";
+import {toast} from "sonner";
+import {useReactFlow} from "@xyflow/react";
 
 
 // ----------------------------------------------------------------------
@@ -10,15 +14,33 @@ import useExecutionPlan from "@/components/hooks/useExecutionPlan";
 const ExecuteBtn = ({workflowId}: { workflowId: string }) => {
 
     const generate = useExecutionPlan();
+    const {toObject} = useReactFlow();
+
+    const mutation = useMutation({
+        mutationFn: RunWorkflow,
+        onSuccess: () => {
+            toast.success("Execution started", {id: "flow-execution"});
+        },
+        onError: () => {
+            toast.error("Something went wrong", {id: "flow-execution"});
+        }
+    });
 
     return (
         <Button
             variant="outline"
             className="flex items-center gap-2"
+            disabled={mutation.isPending}
             onClick={() => {
                 const plan = generate();
-                console.log("--- Plan ---");
-                console.table(plan);
+                if (!plan) {
+                    // Client side validation
+                    return;
+                }
+                mutation.mutate({
+                    workflowId: workflowId,
+                    flowDefinition: JSON.stringify(toObject()),
+                })
             }}
         >
             <PlayIcon size={16} className="stroke-orange-400"/>
--- lib/workflow/executionPlan.ts
+++ lib/workflow/executionPlan.ts
@@ -1,6 +1,6 @@
 import {AppNode, AppNodeMissingInputs} from "@/types/appNode";
 import {WorkflowExecutionPlan, WorkflowExecutionPlanPhase} from "@/types/workflow";
-import {Edge, getIncomers} from "@xyflow/react";
+import {Edge} from "@xyflow/react";
 import {TaskRegistry} from "@/lib/workflow/task/Registry";
 
 // ----------------------------------------------------------------------
@@ -137,4 +137,18 @@ function getInvalidInputs(node: AppNode, edges: Edge[], planned: Set<string>) {
     }
 
     return invalidInputs;
+}
+
+function getIncomers(node: AppNode, nodes: AppNode[], edges: Edge[]) {
+    if (!node.id) {
+        return [];
+    }
+    const incomersIds = new Set();
+    edges.forEach(edge => {
+        if (edge.target === node.id) {
+            incomersIds.add(edge.source);
+        }
+    });
+
+    return nodes.filter(node => incomersIds.has(node.id));
 }
