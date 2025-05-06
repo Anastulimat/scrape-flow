95faf8cec65578c89b8df3408e7c3c03a427583a
Anas TULIMAT <anas.tulimat@orange.com>
2025-05-06 16:49:59 +0200

    feat: add user credit balance model with client ui

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 import {Button, buttonVariants} from "@/components/ui/button";
 import {usePathname} from "next/navigation";
 import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
+import UserAvailableCreditsBadge from "@/components/UserAvailableCreditsBadge";
 
 // ----------------------------------------------------------------------
 
@@ -45,7 +46,9 @@ const DesktopSidebar = () => {
                 <Logo/>
             </div>
 
-            <div className="p-2">TODO CREDITS</div>
+            <div className="p-2">
+                <UserAvailableCreditsBadge/>
+            </div>
 
             <div className="flex flex-col p-2 gap-1">
                 {routes.map(route => (
---
+++ components/UserAvailableCreditsBadge.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import {useQuery} from "@tanstack/react-query";
+import {GetAvailableCredits} from "@/actions/billing/getAvailableCredits";
+import Link from "next/link";
+import {CoinsIcon, Loader2Icon} from "lucide-react";
+import {cn} from "@/lib/utils";
+import ReactCountUpWrapper from "@/components/ReactCountUpWrapper";
+import {buttonVariants} from "@/components/ui/button";
+
+// ----------------------------------------------------------------------
+
+
+function UserAvailableCreditsBadge() {
+    const query = useQuery({
+        queryKey: ["user-available-credits"],
+        queryFn: () => GetAvailableCredits(),
+        refetchInterval: 30 * 1000, // 30 seconds
+    });
+
+    return (
+        <Link href={"/billing"} className={cn(
+            "w-full space-x-2 items-center",
+            buttonVariants({variant: "outline"})
+        )}>
+            <CoinsIcon size={20} className="text-primary"/>
+            <span className="font-semibold capitalize">
+                {query.isLoading && <Loader2Icon className="animate-spin h-4 w-4"/>}
+                {!query.isLoading && query.data && <ReactCountUpWrapper value={query.data}/>}
+                {!query.isLoading && !query.data && "-"}
+            </span>
+        </Link>
+    );
+}
+
+export default UserAvailableCreditsBadge;
