a381f5da054fc09ae503babbccf6f3b46ebe457b
Anas TULIMAT <anas.tulimat@orange.com>
2025-05-06 22:00:39 +0200

    feat: add nextjs loader

--- components/providers/AppProviders.tsx
+++ components/providers/AppProviders.tsx
@@ -3,7 +3,7 @@
 import {ThemeProvider} from "next-themes";
 import {ReactNode, useState} from "react";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import NextTopLoader from "nextjs-toploader";
 
 // ----------------------------------------------------------------------
 
@@ -13,6 +13,7 @@ export function AppProviders({children}: { children: ReactNode }) {
 
     return (
         <QueryClientProvider client={queryClient}>
+            <NextTopLoader color="#10b981" showSpinner={false}/>
             <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
                 {children}
             </ThemeProvider>
